97ecde2977979fc822e2c90b93262d00d3d6888d
Webdev-Chaitanya <webdev.Chaitanya@gmail.com>
2023-04-06 13:45:59 +0530

    featured collection commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import Marquee from "react-fast-marquee";
+import ProductCard from '../components/ProductCard';
 
 const Home = () => {
   return (
@@ -192,7 +193,7 @@ const Home = () => {
         </div>
       </div>
     </section>
-    <section className="marque-wrapper py-5">
+    <section className="marque-wrapper home-wrapper-2 py-5">
       <div className="container-xxl">
         <div className="row">
           <div className="col-12">
@@ -228,6 +229,19 @@ const Home = () => {
               </Marquee>
             </div>
           </div>
+        </div>
+      </div>
+    </section>
+    <section className="blog-wrapper py-5 home-wrapper-2">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="section-heading">Featured Collection</h3>
+          </div>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
         </div>
       </div>
     </section>
