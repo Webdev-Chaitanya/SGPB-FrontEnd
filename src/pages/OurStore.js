b0debf52bf6ca893674d1933c2fc75f2b391d1a8
Webdev-Chaitanya <webdev.Chaitanya@gmail.com>
2023-04-08 12:28:14 +0530

    our store commit2

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import ReactStars from "react-rating-stars-component"
 
 function OurStore() {
   return (
@@ -84,12 +85,94 @@ function OurStore() {
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Product Tags</h3>
+                <div>
+                    <div className="product-tags d-flex flex-wrap align-items-center gap-10">
+                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                            Vati
+                        </span>
+                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                            Panati
+                        </span>
+                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                            supari
+                        </span>
+                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                            kumkum
+                        </span>
+                    </div>
+                </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Random Product</h3>
+                <div>
+                    <div className="random-products mb-3 d-flex">
+                        <div className="w-50">
+                            <img src="images/watch.jpg" className="img-fluid" alt="watch"/>
+                        </div>
+                        <div className="w-50">
+                            <h5>
+                                kids headphone bulk 10 pack
+                            </h5>
+                            <ReactStars
+                                count={5}
+                                size={24}
+                                value={4}
+                                edit={false}
+                                activeColor="#ffd700"
+                            />
+                            <b>$ 300</b>
+                        </div>
+                    </div>
+                    <div className="random-products d-flex">
+                        <div className="w-50">
+                            <img src="images/watch.jpg" className="img-fluid" alt="watch"/>
+                        </div>
+                        <div className="w-50">
+                            <h5>
+                                kids headphone bulk 10 pack
+                            </h5>
+                            <ReactStars
+                                count={5}
+                                size={24}
+                                value={4}
+                                edit={false}
+                                activeColor="#ffd700"
+                            />
+                            <b>$ 300</b>
+                        </div>
+                    </div>
+                </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+                <div className="filter-sort-grid">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="d-flex align-items-center gap-10">
+                            <p className="mb-0 d-block" style={{width:"100px"}}>Sort By</p>
+                            <select name="" className="form-control form-select" id="">
+                                <option value="manual">Features</option>
+                                <option value="best-selling" selected="selected">Best selling</option>
+                                <option value="title-ascending">Alphabetically, A-z</option>
+                                <option value="title-descending">Alphabetically Z-A</option>
+                                <option value="price-ascending">price: low to High</option>
+                                <option value="price-descending">price: high to low</option>
+                                <option value="created-ascending">Date, old to new</option>
+                                <option value="created-descending">Date: new to old</option>
+                            
+                            </select>
+                        </div>
+                        <div className="d-flex align-items-center gap-10">
+                            <p className="totalproducts mb-0">21 Products</p>
+                            <div className="d-flex gap-10 align-items-center grid">
+                                <img src="images/gr4.svg" className="d-block img-fluid" alt="grid"/>    
+                                <img src="images/gr3.svg" className="d-block img-fluid" alt="grid"/>
+                                <img src="images/gr2.svg" className="d-block img-fluid" alt="grid"/>
+                                <img src="images/gr.svg" className="d-block img-fluid" alt="grid"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
           </div>
         </div>
       </div>
